6b09c6325b7dc561bc2a478bdfc884a3f81feaf1
Gil Elbaz <77351401+GilElbaz-Code@users.noreply.github.com>
2024-03-05 00:20:10 +0200

    project structure and requirements.txt change

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify, send_file
 from flask_cors import CORS
-from constants import Constants
-from graph_manager import GraphManager
+from utils.constants import Constants
+from objects.graph_manager import GraphManager
 
 # Create a Flask application instance
 app = Flask(__name__)
@@ -11,6 +11,7 @@ CORS(app, expose_headers=["Content-Disposition"])
 
 # Create an instance of the GraphManager class for managing graphs
 graph_manager = GraphManager()
+
 
 # Define a route for handling POST requests to calculate the shortest path
 @app.route('/calculate_shortest_path', methods=['POST'])
@@ -27,14 +28,14 @@ def calculate_shortest_path():
         # Calculate the shortest path using the GraphManager
         shortest_path = graph_manager.calculate_shortest_path(start=start, end=end, kml_requested=kml_requested)
 
-        # If the result is a string, assume it's a file path and return it as a downloadable KML file
+        # If the result is a string, it's a file path and return it as a downloadable KML file
         if isinstance(shortest_path, str):
             return send_file(shortest_path,
                              mimetype=Constants.KML_MIMETYPE,
                              as_attachment=True,
                              download_name='shortest_path.kml'), 200
 
-        # If the result is not a string, assume it's a list of nodes representing the shortest path and return it as JSON
+        # If the result is not a string, it's a list of nodes representing the shortest path and return it as JSON
         return jsonify({'path': shortest_path}), 200
 
     except Exception as e:
@@ -42,6 +43,6 @@ def calculate_shortest_path():
         error_message = f"An unexpected error occurred: {str(e)}"
         return jsonify({'error': error_message}), 500
 
-# Run the Flask application if this script is executed directly
+
 if __name__ == '__main__':
     app.run()
